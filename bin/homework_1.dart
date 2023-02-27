2a92848340d930dd34f8a1028e6f3a4781f8c016
timoshkkkk <hahatimhaha@gmail.com>
2023-02-27 21:14:14 +0600

    first commit

---
+++ bin/homework_1.dart
@@ -0,0 +1,22 @@
+void main(){
+
+double b = 4.20;
+int c = 3;
+
+print(b+c);
+
+ String text = 'Name';
+text = 'Flutter';
+print(text);
+
+String a = 'I want to learn Flutter';
+
+print(a);
+
+double myNumber=2.225;
+String myText;
+myText = '2.225';
+print(myText);
+
+print(myNumber.toString().runtimeType);
+}
